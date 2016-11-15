a1a786de3629573fe52d5ed92d7b889aab250795
nOxX51 <noxx_51@hotmail.com>
2016-11-15 14:41:14 +0100

    UPDATE button with Toast

--- app/src/main/java/com/noxx/holdall/MainActivity.java
+++ app/src/main/java/com/noxx/holdall/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Toast.makeText(MainActivity.this,"well done",Toast.LENGTH_SHORT).show();
             }
         });
 
