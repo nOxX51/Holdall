61764f854b74e0b66243f2b50f18e9b1a90827c3
nOxX51 <noxx_51@hotmail.com>
2016-11-15 16:00:07 +0100

    UPDATE button 2 into Picasso button

--- app/src/main/java/com/noxx/holdall/MainActivity.java
+++ app/src/main/java/com/noxx/holdall/MainActivity.java
@@ -1,5 +1,6 @@
 package com.noxx.holdall;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -19,6 +20,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this,"well done",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button button2 = (Button) findViewById(R.id.test2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,PhotoActivity.class);
+                startActivity(intent);
             }
         });
 
