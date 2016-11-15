46f52f150ed1c3084df51e9c3f5ed03e8cd495af
nOxX51 <noxx_51@hotmail.com>
2016-11-15 16:38:34 +0100

    ADD new photo JLO

--- app/src/main/java/com/noxx/holdall/PhotoActivity.java
+++ app/src/main/java/com/noxx/holdall/PhotoActivity.java
@@ -1,7 +1,11 @@
 package com.noxx.holdall;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class PhotoActivity extends AppCompatActivity {
 
@@ -9,6 +13,14 @@ public class PhotoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo);
+
+
+
+        String url="http://cdn.images.express.co.uk/img/dynamic/126/590x/secondary/Jennifer-Lopez-220590.jpg";
+        ImageView image = (ImageView) findViewById(R.id.picture);
+
+        Picasso.with(this).load(url).into(image);
+
     }
 
 }
